f68e4bf153238bdc7371a497352461243cf9136a
kendall <kendall.poindexter@detroitlabs.com>
2020-05-27 08:38:08 -0400

    Create UI

---
+++ WeatherAppIota/WeatherAppIota/ForcastTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ForcastTableViewCell.swift
+//  WeatherAppIota
+//
+//  Created by Kendall Poindexter on 5/27/20.
+//  Copyright © 2020 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class ForcastTableViewCell: UITableViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var forcastLowTemp: UILabel!
+    @IBOutlet weak var forcastHighTemp: UILabel!
+}
---
+++ WeatherAppIota/WeatherAppIota/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherAppIota
+//
+//  Created by Kendall Poindexter on 5/27/20.
+//  Copyright © 2020 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var currentLowTemp: UILabel!
+    @IBOutlet weak var currentHighTemp: UILabel!
+    @IBOutlet weak var forcastTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
